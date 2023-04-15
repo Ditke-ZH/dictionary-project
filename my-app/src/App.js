5b12aff7999a99b77f5634101b09dad61587ed6c
Ditke-ZH <edit.goczan@gmail.com>
2023-04-15 18:59:45 +0200

    Added dictionary API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "//public/logo192.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,14 +5,30 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          <p class="footer">
+            <a
+              href="https://github.com/Ditke-ZH/dictionary-project"
+              target="_blank"
+              title="Open-source code"
+              id="personal-link"
+            >
+              Open-source code
+            </a>
+            &nbsp;by&nbsp;
+            <a
+              href="https://www.linkedin.com/in/edit-guntermann-563357104"
+              target="_blank"
+              title="Edit Guntermann"
+              id="personal-link"
+            >
+              Edit Guntermann
+            </a>
+          </p>
         </footer>
       </div>
     </div>
